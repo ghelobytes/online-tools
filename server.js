661725a000add71354ef7357b49e8017172d6637
Angelo Arboleda <ghelobytes@yahoo.com>
2014-06-16 12:56:41 +0800

    Added process.env.OPENSHIFT_NODEJS_HOST and process.env.OPENSHIFT_NODEJS_PORT

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
-var port = process.env.PORT || 8080;
+var host = process.env.OPENSHIFT_NODEJS_HOST ? '0.0.0.0' : '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
